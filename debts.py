2edaba642ce0de544c5b14b60a53525b61aa90e6
Juan Torres <jmmtorres1991@yahoo.com>
2026-08-04 20:56:22 -0500

    lots to do still

---
+++ debts.py
@@ -0,0 +1,18 @@
+
+
+class Debt:
+    def __init__(self,total, monthly_payment, target_payment):
+        self.total = total
+        self.monthly_payment = monthly_payment
+        self.target_payment = target_payment
+        self.months_left = 5
+
+    def __repr__(self):
+        return f"Total Debt: {self.total}\nMonthly_payment: {self.monthly_payment}.\nWith target payment of {self.target_payment} your debt will be settled in {self.months_left} months"
+    
+
+
+def custom_json(obj):
+    if isinstance(obj, complex):
+        return {'__complex__': True, 'real': obj.real, 'imag': ["this","that","the other"]}
+    raise TypeError(f'Cannot serialize object of {type(obj)}')
---
+++ main.py
@@ -0,0 +1,11 @@
+import debts
+
+def main():
+    print("Eventually, this will all be something")
+    test_debt = debts.Debt("31,000","500","600")
+    print(test_debt)
+    is_running = False
+    while is_running:
+        print("is running")
+
+main()
