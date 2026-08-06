8402b70fd260b437f9f54b57ce6c2a73a6f2812b
Juan Torres <jmmtorres1991@yahoo.com>
2026-08-05 20:01:51 -0500

    added variables and updated print

--- debts.py
+++ debts.py
@@ -1,17 +1,18 @@
 
 
 class Debt:
-    def __init__(self,total, monthly_payment, target_payment):
+    def __init__(self,name,total, monthly_payment, target_payment):
+        self.name = name
         self.total = total
         self.monthly_payment = monthly_payment
         self.target_payment = target_payment
         self.months_left = 5
 
     def __repr__(self):
-        return f"Total Debt: {self.total}\nMonthly_payment: {self.monthly_payment}.\nWith target payment of {self.target_payment} your debt will be settled in {self.months_left} months"
+        return f"{self.name} has a Total Debt: {self.total}\nMonthly_payment: {self.monthly_payment}.\nWith target payment of {self.target_payment} your debt will be settled in {self.months_left} months"
     
 
-
+#fix this 
 def custom_json(obj):
     if isinstance(obj, complex):
         return {'__complex__': True, 'real': obj.real, 'imag': ["this","that","the other"]}
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import debts
 
 def main():
     print("Eventually, this will all be something")
-    test_debt = debts.Debt("31,000","500","600")
+    test_debt = debts.Debt("Firstmark","31,000","500","600")
     print(test_debt)
     is_running = False
     while is_running:
